910340bf4f1ba16f2f500e03eb49f9f6b74e0d54
Amirabbas rezaei <abbassxz2@gmail.com>
2023-01-07 15:51:28 +0330

    fix

--- dist/middlewares/isUserAuthed.middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import { Context } from "../context";
-interface IsUserAuthed {
-    next: any;
-    ctx: Context;
-}
-export declare const isUserAuthed: ({ ctx, next }: IsUserAuthed) => Promise<any>;
-export {};
