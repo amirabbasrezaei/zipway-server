cfa37367ee2c7f524e6308fe0df7e4028715ac60
AmirAbbas <amirabbasrzi78@gmail.com>
2024-02-08 10:00:41 +0000

    fix

--- src/utils/getUser.ts
+++ src/utils/getUser.ts
@@ -23,9 +23,6 @@ async function getUserSession(
       where: {
         id: session.userId,
       },
-    }), prisma.user.update({
-      where:{id: session.userId},
-      data:{lastlogin: new Date(Date.now())}
     })]);
     if (user) {
       return { user, session };
