0f20beb828143a53a08a3c50b969cdea4058bc7a
AmirAbbas <amirabbasrzi78@gmail.com>
2024-02-20 16:29:22 +0000

    fix

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -173,7 +173,7 @@ export async function zipwayConfigController({
         "حداقل مبلغ مورد نیاز برای افزایش اعتبار حساب ۲۰۰۰۰ تومان می باشد.",
       logoutAppText: "؟آیا برای خروج از حساب کاربری خود مطمئنید",
       maxCreatePayment: 50000000,
-      minCreatePayment: 10000,
+      minCreatePayment: 1000,
       privacyPolicyText: "",
       rideWaiting: {
         image: {
