14b4458a776575947274818283cb54e4c1ede9e7
AmirAbbas <amirabbasrzi78@gmail.com>
2024-02-22 21:54:48 +0330

    fix

--- src/requests/baladAPIs.ts
+++ src/requests/baladAPIs.ts
@@ -7,11 +7,9 @@ const BASE_URL = 'https://search.raah.ir/v6/'
 export async function AddresToCoordinate({latitude, longitude, searchTerm, polygon=[], zoom=0}:PlaceBaseSearchArgs) {
     try {
         const response = await axios.get(`${BASE_URL}?text=${searchTerm}&polygon=${polygon.length ? polygon.map((loc) => `${loc.x},${loc.y}`).join(',') : ""}&zoom=${zoom ? zoom : ""}&camera=${longitude},${latitude}`)
-        console.log(polygon.map((loc) => `${loc.x},${loc.y}`))
-        console.log(response.data.results[0].geometry)
         return response.data
     } catch (error) {
-        // console.log(error)
+        console.log(error)
     }
     
     
