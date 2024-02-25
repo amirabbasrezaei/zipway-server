6beae2cf521d9c1cc2a29738c7ace9a6368acfb8
AmirAbbas <amirabbasrzi78@gmail.com>
2024-02-25 12:36:44 +0330

    fix

--- src/controllers/panel.controller.ts
+++ src/controllers/panel.controller.ts
@@ -1,9 +1,23 @@
 import { TRPCError } from "@trpc/server";
 import { RouterArgsController } from "./type";
 import { z } from "zod";
-import {UserModel} from "../../prisma/zod/user.ts"
 
 
+export const UserModel = z.object({
+  id: z.string(),
+  name: z.string(),
+  familyName: z.string().nullish(),
+  password: z.string().nullish(),
+  email: z.string().nullish(),
+  phoneNumber: z.string(),
+  role: z.enum(["USER", "SUPERADMIN"]),
+  loginCode: z.string().nullish(),
+  isVerified: z.boolean(),
+  credit: z.number().int(),
+  createdAt: z.date(),
+  lastLogin: z.date().nullish(),
+  numberOfLogins: z.number().int().nullish(),
+})
 
 export const getUsersControllerArgsTypeSchema = z.object({
   orderBy: z.enum(UserModel.keyof()._def.values),
@@ -16,7 +30,7 @@ export async function getUsersController({
   ctx,input
 }: RouterArgsController<getUserArgsType>) {
   const { prisma , user} = ctx;
-  if(user.role !== "SUPERUSER"){
+  if(user.role !== "SUPERADMIN"){
        throw new TRPCError({code: "UNAUTHORIZED"})
   }
 
